d6fc489a759dae2b6bbd6d48ad54988d53a61348
belly-rewardz <dekri@rewardz.sg>
2023-08-15 17:58:24 +0700

    first init

---
+++ handler.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports.hello = async (event) => {
+  // Read an environment variable
+  const apiKey = process.env.API_KEY;
+  console.log(apiKey);
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v3.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
